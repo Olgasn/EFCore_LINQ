a49b91f307f7830b769a43b52a4e34ef5439540b
Asenchik Oleg <olgasn@inbox.ru>
2025-10-01 19:42:58 +0300

    Revert "Codespaces"
    This reverts commit 738e81bde195d5ee2c55f93132e26e7f273c1a9d.

--- .devcontainer/wait-for-mssql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-echo "ќжидание запуска SQL Server..."
-for i in {1..30}; do
-    /opt/mssql-tools/bin/sqlcmd -S mssql -U sa -P "$MSSQL_SA_PASSWORD" -Q "SELECT 1" && exit 0
-    sleep 2
-done
-echo "SQL Server не запущен!"
-exit 1
