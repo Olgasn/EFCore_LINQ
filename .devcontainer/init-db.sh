50813e83b23a43300fdef864ff75297960202102
Asenchik Oleg <olgasn@inbox.ru>
2025-10-02 16:44:32 +0300

    Codespaces

--- .devcontainer/init-db.sh
+++ .devcontainer/init-db.sh
@@ -3,11 +3,11 @@ set -e
 
 # Ждём, пока SQL Server будет готов принимать подключения
 echo "Waiting for SQL Server to be ready..."
-until /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" -Q "SELECT 1" > /dev/null 2>&1; do
+until /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "12345678_N" -Q "SELECT 1" > /dev/null 2>&1; do
   echo "SQL Server is not ready yet, waiting..."
   sleep 5
 done
 
 echo "SQL Server is ready. Creating the database..."
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" -i /tmp/setup-database.sql
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "12345678_N" -i /tmp/setup-database.sql
 echo "Инициализация завершена."
